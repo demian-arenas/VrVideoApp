b18722c1806147940127290f3b0b47455d91324a
Demian Arenas <arenasdemian99@gmail.com>
2017-11-28 19:34:08 -0600

    [update] implement updateScene function

--- components/scenes/layouts/elements/Button.js
+++ components/scenes/layouts/elements/Button.js
@@ -37,32 +37,39 @@ class Button extends React.Component {
   }
 
   render() {
+    const showButton = this.props.showButton
     return (
-      <Animated.View
-        style={{
-          margin: 0.1,
-          paddingLeft: 0.2,
-          paddingRight: 0.2,
-          height: 0.3,
-          backgroundColor: '#A482DF',
-          borderRadius: 0.1,
-          opacity: this.state.fadeIn,
-          transform: [
-            { translateX: this.state.slideRight }
-          ]
-        }}
-      >
-        <VrButton>
-          <Text
+      <View>
+        {showButton ? (
+          <Animated.View
             style={{
-              fontSize: 0.2,
-              textAlign: 'center',
-              color: "#FFFFFF"
-            }}>
-            {this.props.text}
-          </Text>
-        </VrButton>
-      </Animated.View>
+              margin: 0.1,
+              paddingLeft: 0.2,
+              paddingRight: 0.2,
+              height: 0.3,
+              backgroundColor: '#A482DF',
+              borderRadius: 0.1,
+              opacity: this.state.fadeIn,
+              transform: [
+                { translateX: this.state.slideRight }
+              ]
+            }}
+          >
+            <VrButton onClick={this.props.updateScene}>
+              <Text
+                style={{
+                  fontSize: 0.2,
+                  textAlign: 'center',
+                  color: "#FFFFFF"
+                }}>
+                {this.props.text}
+              </Text>
+            </VrButton>
+          </Animated.View>
+        ) : (
+            <View></View>
+          )}
+      </View>
     )
   }
 }
