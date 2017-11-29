4a18ae353fa31e01584a2a56280a65f6265fb320
Demian Arenas <arenasdemian99@gmail.com>
2017-11-28 21:15:36 -0600

    [update] pass changeScene and scene by props on TitleScene

--- components/scenes/TitleScene.js
+++ components/scenes/TitleScene.js
@@ -6,16 +6,24 @@ import {
   asset,
   Pano
 } from 'react-vr'
+
 import TitleLayout from './layouts/TitleLayout.js'
+
 //Scene
 class TitleScene extends React.Component {
   render() {
     return (
       <View>
         <Pano source={asset('title-background.jpg')}/>
-        <TitleLayout text={this.props.text} />
-      </View>
+        <TitleLayout
+          showButton={this.props.showButton}
+          text={this.props.text}
+          changeScenes={this.props.changeScenes}
+          scene={this.props.scene}
+        />
+     </View>
     )
   }
 }
+
 module.exports = TitleScene
