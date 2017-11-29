52ee1dd36701d8bfc7aca8fb93ce004137d55570
Demian Arenas <arenasdemian99@gmail.com>
2017-11-28 21:11:59 -0600

    [update] send scene and changeScenes by props on TitleLayout

--- components/scenes/layouts/TitleLayout.js
+++ components/scenes/layouts/TitleLayout.js
@@ -4,25 +4,33 @@ import {
   View,
   VrButton
 } from 'react-vr'
+
 import Title from './elements/Title.js'
 import Button from './elements/Button.js'
+
+
 //Layout
 class TitleLayout extends React.Component {
   render() {
     return (
-      <View
-        style={{
-          width: 2,
-          flexDirection: 'column',
-          alignItems: 'stretch',
-          justifyContent: 'center',
-          layoutOrigin: [0.5, 0.5],
-          transform: [{ translate: [0, 0, -3] }]
-        }}>
-        <Title />
-        <Button showButton={this.props.showButton} text={this.props.text} />
+      <View style={{
+        width: 2,
+        flexDirection: 'column',
+        alignItems: 'stretch',
+        justifyContent: 'center',
+        layoutOrigin: [0.5, 0.5],
+        transform: [{translate: [0, 0, -3]}]
+      }}>
+        <Title/>
+        <Button
+          showButton={this.props.showButton}
+          text={this.props.text}
+          changeScenes={this.props.changeScenes}
+          scene={this.props.scene}
+        />
       </View>
     )
   }
 }
+
 module.exports = TitleLayout
