134227a59ce8897be820c6bcac0ae627f0fc201b
Demian Arenas <arenasdemian99@gmail.com>
2017-11-28 21:14:51 -0600

    [update] set streamURL state and pass props to VideoPlayerLayout

--- components/scenes/VideoPlayer.js
+++ components/scenes/VideoPlayer.js
@@ -5,14 +5,32 @@ import {
   asset,
   Pano
 } from 'react-vr'
+
 import VideoPlayerLayout from './layouts/VideoPlayerLayout.js'
+
 //Scene
 class VideoPlayer extends React.Component {
+  constructor() {
+    super()
+    this.state = { streamURL: "" }
+  }
+
+  componentWillMount() {
+    this.setState({ streamURL: 'http://playertwitch.tv/?channel=' + this.props.streamID })
+    //example: http://player.twitch.tv/?channel=beyondthesummit
+  }
+
   render() {
     return (
       <View>
-        <Pano source={asset('title-background.jpg')} />
-        <VideoPlayerLayout showButton={this.props.showButton} text={this.props.text} />
+        <Pano source={asset(this.props.env)}/>
+        <VideoPlayerLayout
+          streamURL={this.state.streamURL}
+          showButton={this.props.showButton}
+          text={this.props.text}
+          changeScenes={this.props.changeScenes}
+          scene={this.props.scene}
+        />
       </View>
     )
   }
