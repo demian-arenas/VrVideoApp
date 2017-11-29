936643b556401fa30fb6154d65d891d0c1152e8a
Demian Arenas <arenasdemian99@gmail.com>
2017-11-28 21:13:47 -0600

    [update] send streamURL and changeSceneby props on VideoPlayer

--- components/scenes/layouts/VideoPlayerLayout.js
+++ components/scenes/layouts/VideoPlayerLayout.js
@@ -2,10 +2,13 @@ import React from 'react'
 import {
   View
 } from 'react-vr'
+
 import VideoElement from './elements/VideoElement.js'
 import Button from './elements/Button.js'
+
 //Layout
 class VideoPlayerLayout extends React.Component {
+
   render() {
     return (
       <View>
@@ -16,22 +19,29 @@ class VideoPlayerLayout extends React.Component {
           alignItems: 'stretch',
           backgroundColor: '#333333',
           layoutOrigin: [0.5, 0.5],
-          transform: [{ translate: [0, 0, -5] }]
+          transform: [{translate: [0, 0, -6]}]
         }}>
-          <VideoElement />
+          <VideoElement streamURL={this.props.streamURL}/>
         </View>
+
         <View style={{
           flex: 1,
           width: 2.5,
           flexDirection: 'column',
           alignItems: 'stretch',
           layoutOrigin: [0.5, 0.5],
-          transform: [{ translate: [0, 3.5, 5] }, { rotateY: -180 }]
+          transform: [{translate: [0, 3.5, 5]}, {rotateY: -180}]
         }}>
-          <Button showButton={this.props.showButton} text={this.props.text} />
+          <Button
+            showButton={this.props.showButton}
+            text={this.props.text}
+            changeScenes={this.props.changeScenes}
+            scene={this.props.scene}
+          />
         </View>
       </View>
     )
   }
 }
+
 module.exports = VideoPlayerLayout
